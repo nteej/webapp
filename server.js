e3cacd051dff390fb1acd83e50b62193814ac391
Tharanga Jayasinghe <nteeje@gmail.com>
2018-03-17 11:36:38 +0530

    Implement Express Framework:Update port

--- server.js
+++ server.js
@@ -21,5 +21,5 @@ app.get('/about', function (req, res) {
     res.render('pages/about');
 });
 
-app.listen(8080);
-console.log('8080 is the magic port');
+app.listen(8000);
+console.log('8000 is the magic port');
